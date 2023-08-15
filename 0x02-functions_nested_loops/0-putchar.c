9c2c686c66387f6dab037e422cd64c62a13b0089
Stay <staynjokede@gmail.com>
2023-08-15 16:52:31 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+int main(void)
+{
+    int i;
+    char str[] = "_putchar\n";
+
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        _putchar(str[i]);
+    }
+
+    return (0);
+}
