05027f8724a7e8cd4a28bbc26d2e1e889bcd1373
Stay <staynjokede@gmail.com>
2023-08-15 22:25:04 -0700

    fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,22 +8,22 @@
  */
 int main(void)
 {
-    int i;
-    long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 2;
+	int i;
+	long int fib1 = 1, fib2 = 2, fib_sum = 0, even_sum = 2;
 
-    for (i = 3; fib_sum <= 4000000; i++)
-    {
-        fib_sum = fib1 + fib2;
-        fib1 = fib2;
-        fib2 = fib_sum;
+	for (i = 3; fib_sum <= 4000000; i++)
+	{
+		fib_sum = fib1 + fib2;
+		fib1 = fib2;
+		fib2 = fib_sum;
 
-        if (fib_sum % 2 == 0)
-        {
-            even_sum += fib_sum;
-        }
-    }
+		if (fib_sum % 2 == 0)
+		{
+			even_sum += fib_sum;
+		}
+	}
 
-    printf("%ld\n", even_sum);
+	printf("%ld\n", even_sum);
 
-    return 0;
+	return (0);
 }
