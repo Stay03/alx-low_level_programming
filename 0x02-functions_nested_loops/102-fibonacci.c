753d6cf9300ced9ec07851b0d85cbcab455fa10e
Stay <staynjokede@gmail.com>
2023-08-15 21:56:22 -0700

    fib

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    long int n1 = 1, n2 = 2, next;
+    int i;
+
+    printf("%ld, %ld", n1, n2);
+
+    for (i = 3; i <= 50; i++)
+    {
+        next = n1 + n2;
+        printf(", %ld", next);
+        n1 = n2;
+        n2 = next;
+    }
+
+    printf("\n");
+
+    return (0);
+}
