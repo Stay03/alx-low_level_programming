ee6549c0ebb6fc8d7039e92b88c954b62f2d0136
Stay <staynjokede@gmail.com>
2023-08-15 22:36:12 -0700

    its a wrap

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,48 @@
+#include "main.h"
+#include <stdio.h>
+
+void print_fibonacci(void);
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	print_fibonacci();
+	putchar('\n');
+	return (0);
+}
+
+void print_fibonacci(void)
+{
+	int count;
+	unsigned long int prev, current, next;
+
+	count = 2;
+	prev = 1;
+	current = 2;
+
+	putchar('1');
+	putchar(',');
+	putchar(' ');
+	putchar('2');
+	putchar(',');
+
+	while (count < 98)
+	{
+		next = prev + current;
+		printf(" %lu", next);
+
+		prev = current;
+		current = next;
+
+		count++;
+		if (count != 98)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+}
