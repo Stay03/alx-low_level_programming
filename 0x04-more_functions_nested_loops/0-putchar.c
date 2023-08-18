5a0350369bedcb53c6576066419352deaab1200a
Stay <staynjokede@gmail.com>
2023-08-17 17:29:09 -0700

    print Numbers

---
+++ 0x04-more_functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char str[] = "_putchar\n";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+
+	return (0);
+}
