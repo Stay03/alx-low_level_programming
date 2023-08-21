651dcc6cb29ed1ea9e79cd7820a22e46f0ec87a4
Stay <staynjokede@gmail.com>
2023-08-21 15:18:51 -0700

    100-atoi

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,14 +12,18 @@ int _atoi(char *s)
 	int result = 0;
 	int i = 0;
 
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+
 	while (s[i] != '\0')
 	{
-		if (s[i] == '-')
-			sign *= -1;
-		if ((s[i] >= '0') && (s[i] <= '9'))
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			result = result * 10 + (s[i] - '0');
-			if (!((s[i + 1] >= '0') && (s[i + 1] <= '9')))
+			if (s[i + 1] < '0' || s[i + 1] > '9')
 				break;
 		}
 		i++;
@@ -27,4 +31,3 @@ int _atoi(char *s)
 
 	return (result * sign);
 }
-
