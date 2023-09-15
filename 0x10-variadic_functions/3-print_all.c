0ef993952d125d029eba04742af65c08daaa3705
Stay <staynjokede@gmail.com>
2023-09-14 18:56:43 -0700

    3-print_all.cwq

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - prints all
+ * @format: format
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str;
+	int i = 0;
+
+	va_start(args, format);
+	while (format == NULL)
+	{
+		printf("\n");
+		return;
+	}
+	while (format[i])
+	{
+		switch (format[i++])
+		{
+			case 'c':
+				printf("%c", va_arg(args, int));
+				break;
+			case 'i':
+				printf("%d", va_arg(args, int));
+				break;
+			case 'f':
+				printf("%f", va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (str == NULL)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+			default:
+				continue;
+		}
+		if (format[i])
+			printf(", ");
+	}
+	printf("\n");
+	va_end(args);
+}
