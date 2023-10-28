f4145b916bcdbb28d6b974ef4b3ada04c04e1775
root <staynjokede@gmail.com>
2023-10-27 23:39:30 -0700

    all

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: The number of command-line arguments
+ * @argv: An array of command-line argument strings
+ *
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* Unused parameter */
+	printf("%s\n", argv[0]);
+	return (0);
+}
+
