94a6ce1a0d813c236ae7374e9336c6faca766906
root <staynjokede@gmail.com>
2023-11-27 17:31:37 -0800

    100

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,11 +2,29 @@
 #define MAIN_H
 
 #include <stdlib.h>
+#include <stddef.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <elf.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdio.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
 size_t strlen(const char *str);
+
+void check_elf(unsigned char *e_ident);
+void print_elf_info(Elf64_Ehdr *header);
+void close_elf(int elf);
+int main(int __attribute__((__unused__)) argc, char *argv[]);
 
 #endif /* MAIN_H */
