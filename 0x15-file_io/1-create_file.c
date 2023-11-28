efe11655a8c0bdfcd162ce5aa908ee45786e78d6
root <staynjokede@gmail.com>
2023-11-27 17:02:52 -0800

    02

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	/* Open the file with the required permissions, create it if it doesn't exist, and truncate if it does */
+	/* Open the file with the permissions, and truncate if it does */
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
